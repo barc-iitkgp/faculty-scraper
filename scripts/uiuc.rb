67f03ee5629837ad6c624bed57d333fb36444c6e
sgdgp <sgdgp.2014@gmail.com>
2016-12-02 19:25:00 +0530

    Removed empty lines from uiuc.rb

--- scripts/uiuc.rb
+++ scripts/uiuc.rb
@@ -3,14 +3,10 @@ bot , filename = mechanize_bot($0)
 csv_write(filename,"NAME","EMAIL ID","COURSES / AREA")
 
 page = bot.get("http://illinois.edu/ds/detail?departmentId=illinois.eduKP434")
-
 num = page.search(".ws-ds-table-col1").count
-
 for i in (0..num-1)
     name = page.search(".ws-ds-table-col1")[i].text.strip.to_s
     name = name.chomp(',')
-   
-    
     email = page.search(".ws-ds-table-col3")[i].text.strip.to_s
     email = email[17..-3]
     email_ender = "@illinois.edu"
@@ -18,6 +14,4 @@ for i in (0..num-1)
     dept = "Department of Computer Science"
     csv_write(filename,name,email,dept)
     puts (i+1).to_s + "/" + num.to_s + " completed"
-   
-
 end
