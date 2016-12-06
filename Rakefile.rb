2836230b8ea83e1effc07f9247cb806eeb2b911b
athityakumar <athityakumar@gmail.com>
2016-12-06 17:15:06 +0530

    Updated Rakefile for Travis CI check

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,3 +1,6 @@
 Dir.chdir(Dir.pwd+"/scripts/")
-system "ruby mit.rb;"
+ruby_scripts = Dir.entries(".").keep_if {|a| a.end_with? ".rb" }
+ruby_scripts.each do |r|
+    system "ruby #{r};"
+end
 exit(0)
