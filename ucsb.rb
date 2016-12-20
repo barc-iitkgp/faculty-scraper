1196b086a00e21ce82c4cd41be2bc7a4212a3e1e
Dhruv Apte <dhruvgirishapte@gmail.com>
2016-12-20 22:14:59 +0530

    Added UCSB (#11)
    * Added Yale
    
    * added Yale
    
    * Update README.md
    
    * Added UCSB
    
    * Added UCSB

---
+++ ucsb.rb
@@ -0,0 +1,20 @@
+require_relative 'include/bot'
+bot , filename = mechanize_bot($0)
+csv_write(filename,"NAME","MAIL ID","PAGE")
+
+mainpage = bot.get("http://www.cs.ucsb.edu/people/")
+num , j = mainpage.search(".found").text.gsub("Number of people found: ","").to_i , 1
+pagination = num%20 == 0 ? (num/20) : (num/20) + 1
+for i in (1..pagination)
+    page = bot.get("http://www.cs.ucsb.edu/people/#{i.to_s}")
+    page.search("table th").each do |person|
+        username = person.children.children.children.last["href"]
+        username = username[27..username.length-2]
+        name = person.children.children.children.last.text
+        link = "http://www.cs.ucsb.edu/people/#{username}"
+        email = "#{username}@cs.ucsb.edu".gsub(" ","")
+        csv_write(filename,name,email,link)
+        puts j.to_s + "/" + num.to_s + " completed"
+        j = j+1
+    end
+end
